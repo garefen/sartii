1c7c23d7c14ab2aee354f3c602c73a34a3529e12
Gabriel Sarti <gabrielbsarti@hotmail.com>
2024-11-12 18:36:14 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="projects" element={<Projects />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Contact.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+function Contact() {
+  return (
+    <div className="home">
+      <div className='home__container'>
+        <div className='home__left'>
+          <Link to="/" className='home__bio'>
+            <h3 className='home__name'>Gabriel Sarti</h3>
+            <div className='home__title'>Front End Software Engineer</div>
+          </Link>
+          <div className='home__links'>
+            <Link to="/" className='home__link'>Home</Link>
+            <Link to="/projects" className='home__link'>Projetos</Link>
+            <Link to="/contact" className='home__link'>Contato</Link>
+          </div>
+
+          <div className='home__social'>
+            <a href='https://www.linkedin.com/in/gabriel-sarti-7aa5ab145/'>Linkedin</a>
+            <a href='https://github.com/garefen'>Github</a>
+          </div>
+        </div>
+        <div className='home__right'>
+          <div className="contact">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
